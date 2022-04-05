3e2b393400bce78f67665a4350765319a9bd84b6
Paula Sierra <paulacode01@gmail.com>
2022-04-05 07:50:53 -0500

    Author model and controller without entity framework

---
+++ WebApiAuthors/Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAuthors.Entity;
+
+namespace WebApiAuthors.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController:ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Author>> Get()
+        {
+            return new List<Author> { 
+            new Author() { Id = 1, Name = "Paula" },
+            new Author() { Id = 2, Name = "David" }
+
+            };
+        }
+    }
+}
